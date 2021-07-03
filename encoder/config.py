7cc52afbe6a5632527e7e19b54049f024744e29e
Prem <rawatpremsingh999@gmail.com>
2021-07-03 12:18:41 +0530

    add audio preprocessor files

---
+++ encoder/config.py
@@ -0,0 +1,14 @@
+librispeech_datasets = {
+    "train": {
+        "clean": ["LibriSpeech/dev-clean"],
+        "other": ["LibriSpeech/dev-other"]
+    },
+    "test": {
+        "clean": ["LibriSpeech/dev-clean"],
+        "other": ["LibriSpeech/dev-other"]
+    },
+    "dev": {
+        "clean": ["LibriSpeech/dev-clean"],
+        "other": ["LibriSpeech/dev-other"]
+    },
+}
